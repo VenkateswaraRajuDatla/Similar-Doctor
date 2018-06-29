bf59fc75708745f833129dbffa82830660638c30
Venkateswara Raju Datla <dvrajudatla1995@gmail.com>
2018-06-29 16:11:44 -0700

    modified mongoose file

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,4 +1,4 @@
 var mongoose=require('mongoose');
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/Doctors');
+mongoose.connect( process.env.MONGODB_URI);
 
 module.exports={mongoose};
